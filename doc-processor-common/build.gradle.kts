bec03a9f225e71158f8943d9c4b8c526786282c4
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-03-27 22:01:27 +0200

    1.2.0 release

--- doc-processor-common/build.gradle.kts
+++ doc-processor-common/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "nl.jolanrensen.docProcessor"
-version = "0.1.2-SNAPSHOT"
+version = "0.1.2"
 
 repositories {
     mavenCentral()
--- doc-processor-gradle-plugin/build.gradle.kts
+++ doc-processor-gradle-plugin/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 
 group = "nl.jolanrensen.docProcessor"
-version = "0.1.2-SNAPSHOT"
+version = "0.1.2"
 
 publishing {
     repositories {
