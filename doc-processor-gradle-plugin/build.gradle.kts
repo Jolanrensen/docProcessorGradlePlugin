ecda55881026a3cc531c59fbd0efb4ce89895055
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-03-28 12:42:48 +0200

    Updating versions

--- doc-processor-gradle-plugin/build.gradle.kts
+++ doc-processor-gradle-plugin/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 
 group = "nl.jolanrensen.docProcessor"
-version = "0.1.3-SNAPSHOT"
+version = "0.1.5-SNAPSHOT"
 
 publishing {
     repositories {
--- doc-processor-gradle-plugin/src/functionalTest/kotlin/nl/jolanrensen/docProcessor/DocProcessorFunctionalTest.kt
+++ doc-processor-gradle-plugin/src/functionalTest/kotlin/nl/jolanrensen/docProcessor/DocProcessorFunctionalTest.kt
@@ -11,7 +11,7 @@ import java.io.IOException
 
 abstract class DocProcessorFunctionalTest(name: String) {
 
-    protected val version = "0.1.3-SNAPSHOT"
+    protected val version = "0.1.5-SNAPSHOT"
 
     init {
         println("NOTE!! make sure you have the plugin installed in your local maven repo")
