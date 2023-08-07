5ebea2f8feb6ac353f87ac15bea110b548519922
Jolan Rensen <jolan.rensen@jetbrains.com>
2023-08-07 13:41:45 +0200

    setting version to 0.2.2

--- doc-processor-gradle-plugin/src/functionalTest/kotlin/nl/jolanrensen/docProcessor/DocProcessorFunctionalTest.kt
+++ doc-processor-gradle-plugin/src/functionalTest/kotlin/nl/jolanrensen/docProcessor/DocProcessorFunctionalTest.kt
@@ -11,7 +11,7 @@ import java.io.IOException
 
 abstract class DocProcessorFunctionalTest(name: String) {
 
-    protected val version = "0.2.2-SNAPSHOT"
+    protected val version = "0.2.2"
 
     init {
         println("NOTE!! make sure you have the plugin installed in your local maven repo")
